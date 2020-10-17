95b303980dc750d9e1105851873e6e8427c98e5c
yangqiangcode <1304397531@qq.com>
2020-10-18 00:00:20 +0800

    加入双向数据绑定实现

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,53 @@
 import { history } from 'umi';
+import React from 'react';
 
+/**
+ * @author 林间有风Lin
+ * @version 1.0
+ * @description 双向数据绑定算法
+ */
+React.rModel = (state, point) => {
+  let once = false;
 
+  if (!once) {
+    // 性能优化,每次组件加载完毕,只会进行一次数据加载
+    const all = document.getElementsByTagName('*'); // 获取所有元素
+    const cpState = { ...point.state }; // 复制一个监测对象
+    let finalStr = null; // 避免null
+
+    Array.from(all).forEach(item => {
+      // 拿到每个元素
+      let stateStr = item.getAttribute('r-model'); // 拿到r-model属性
+      if (stateStr) finalStr = stateStr;
+      if (item instanceof HTMLInputElement && finalStr) {
+        // 如果是input表单，则设置事件
+        item.onkeyup = e => {
+          cpState[finalStr] = e.target.value; // 触发监测对象
+        };
+      }
+
+      if (stateStr) {
+        const o = point.state;
+        let temp = null;
+        Object.defineProperty(cpState, stateStr, {
+          get() {
+            return 123444; // 赋予初始值
+          },
+          set: value => {
+            if (temp === value) return; // 避免死循环
+            temp = value;
+            point.state.data = temp;
+            point.forceUpdate();
+          },
+        });
+      }
+    });
+
+    once = true;
+  }
+};
 
 // 阻塞渲染器
 // develop分支
-const render =  (oldRender) => oldRender() 
-export {render}
-
-
+const render = oldRender => oldRender();
+export { render };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,17 +24,20 @@ class APP extends Component {
     super(props);
     this.state = {
       tag: [],
+      data: 123,
     };
+    this.once = false;
     this.searchValue = React.createRef();
-    this.save = '';
   }
 
   /**
    * @author 林间有风Lin
    * @version 1.0
-   * @description 绑定事件
+   * @description 双向数据绑定实现
    */
-  componentDidMount = () => {};
+  componentDidMount = () => {
+    React.rModel(this.state, this); // 注册r-model指令
+  };
 
   /**
    * @author 林间有风Lin
@@ -70,112 +73,107 @@ class APP extends Component {
    */
   getSearchList = () => {};
 
-  stratSearch = () => {
-    console.log(this.searchValue.current.getAttribute('r-model'));
-  };
-  render() {
-    // const { tag } = this.state
-    const { tag } = this.props.SearchListModel.list;
+  stratSearch = () => {};
 
+  render() {
+    const { data } = this.state;
+    const { tag } = this.props.SearchListModel.list;
     return (
       // nav
-      <div className={`${styles.nav} nv`}>
-        <NavBg />
-        <div className={styles.navTabline}>
-          <div className={styles.navWrap}>
-            <ul className={styles.navLink}>
-              <li>
-                <a href="">
-                  <i className={styles.iconfont}>&#xe62d;</i>主站
-                </a>
-              </li>
-              <li>
-                <a href="">番剧</a>
-              </li>
-              <li>
-                <a href="">游戏中心</a>
-              </li>
-              <li>
-                <a href="">直播</a>
-              </li>
-              <li>
-                <a href="">会员购</a>
-              </li>
-              <li>
-                <a href="">漫画</a>
-              </li>
-              <li>
-                <a href="">赛事</a>
-              </li>
-              <li>
-                <a href="">S10</a>
-              </li>
-              <li>
-                <a href="">
-                  <i className={styles.iconfont}>&#xe620;</i>下载APP
-                </a>
-              </li>
-            </ul>
-            {/* 搜索框 */}
-            <div className={styles.searchForm}>
-              <div className={styles.searchWrap}>
-                <Input
-                  r-model={this.save}
-                  ref={this.searchValue}
-                  onChange={this.searchFormInput}
-                  onBlur={e => {
-                    if (this.searchValue?.current) {
-                      this.searchValue.current.setAttribute(
-                        'r-model',
-                        e.target.value,
-                      );
-                    }
-
-                    this.save = e;
-                  }}
-                  disableUnderline={true}
-                  placeholder="awdwadwa"
-                  className={styles.searchFormInput}
-                />
-                <div className={styles.historyForm}>
-                  {tag?.length > 0
-                    ? tag.map(item => (
-                        <p key={item.value} className={styles.historyItem}>
-                          {item.value}
-                        </p>
-                      ))
-                    : null}
+      <>
+        <div className={`${styles.nav} nv`}>
+          <NavBg />
+          <div className={styles.navTabline}>
+            <div className={styles.navWrap}>
+              <ul className={styles.navLink}>
+                <li>
+                  <a href="">
+                    <i className={styles.iconfont}>&#xe62d;</i>主站
+                  </a>
+                </li>
+                <li>
+                  <a href="">番剧</a>
+                </li>
+                <li>
+                  <a href="">游戏中心</a>
+                </li>
+                <li>
+                  <a href="">直播</a>
+                </li>
+                <li>
+                  <a href="">会员购</a>
+                </li>
+                <li>
+                  <a href="">漫画</a>
+                </li>
+                <li>
+                  <a href="">赛事</a>
+                </li>
+                <li>
+                  <a href="">S10</a>
+                </li>
+                <li>
+                  <a href="">
+                    <i className={styles.iconfont}>&#xe620;</i>下载APP
+                  </a>
+                </li>
+              </ul>
+              {/* 搜索框 */}
+              <div className={styles.searchForm}>
+                <div className={styles.searchWrap}>
+                  <Input
+                    r-model="data"
+                    // value={this.state.data}
+                    ref={this.searchValue}
+                    // onChange={this.searchFormInput}
+                    disableUnderline={true}
+                    placeholder="awdwadwa"
+                    className={styles.searchFormInput}
+                  />
+                  <div className={styles.historyForm}>
+                    {tag?.length > 0
+                      ? tag.map(item => (
+                          <p key={item.value} className={styles.historyItem}>
+                            {item.value}
+                          </p>
+                        ))
+                      : null}
+                  </div>
                 </div>
-              </div>
-              <button onClick={this.stratSearch}>
-                <i className={styles.iconfont}>&#xe616;</i>
-              </button>
-            </div>
-            {/* 用户登陆注册 */}
-            <div className={styles.navUser}>
-              <div className={styles.searchMIN}>
-                <a href="">
+                <button onClick={this.stratSearch}>
                   <i className={styles.iconfont}>&#xe616;</i>
-                </a>
+                </button>
               </div>
-              <div className={styles.userImg}>
-                <a href="">
-                  <img src={require('@public/img/默认头像.png')} alt="" />
-                </a>
-              </div>
-              <div>
-                <a href="">登陆</a>
-              </div>
-              <div>
-                <a href="">注册</a>
-              </div>
-              <div>
-                <button>投稿</button>
+              {/* 用户登陆注册 */}
+              <div className={styles.navUser}>
+                <div className={styles.searchMIN}>
+                  <a href="">
+                    <i className={styles.iconfont}>&#xe616;</i>
+                  </a>
+                </div>
+                <div className={styles.userImg}>
+                  <a href="">
+                    <img src={require('@public/img/默认头像.png')} alt="" />
+                  </a>
+                </div>
+                <div>
+                  <a href="">登陆</a>
+                </div>
+                <div>
+                  <a href="">注册</a>
+                </div>
+                <div>
+                  <button>投稿</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        {data}
+        <p>{data}</p>
+        <p>{data}</p>
+        <p>{data}</p>
+      </>
     );
   }
 }
